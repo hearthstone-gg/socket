3127e108dd47ce29554e36c0adae553ab8ed6781
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-05-23 01:27:28 -0400

    add config

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ require('pmx').init();
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = 3003;
+
+var argv = require('yargs').argv;
+var config = require('hs.gg-config').get(argv.env || 'local').services.socket;
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
+	// setInterval(function() {
+	// 	socket.emit('ping', Math.random());
+	// },10000);
 });
 
-http.listen(port, function() {
+http.listen(config.port, function() {
 	var host = http.address().address;
 	var port = http.address().port;
 
